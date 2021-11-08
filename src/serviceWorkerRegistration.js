d8867fe5753220054f12d1b258a8e52fe887e20d
Humayon Zafar <humayonhere@gmail.com>
2021-11-08 21:36:38 +0500

    Console for debugging

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -31,7 +31,7 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+      console.log(process.env.PUBLIC_URL, config, checkValidServiceWorker(swUrl, config));
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
