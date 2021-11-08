20f3236a94d57e89ed806b2a6ba7965dbfb3c1c6
Humayon Zafar <humayonhere@gmail.com>
2021-11-09 00:15:49 +0500

    Fixed close button issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,10 @@ const Header = () => {
     const [showSocialMenu, toggleShowSocialMenu] = useState(false);
 
     const closeTab = (e) => {
+        window.opener = null;
+        window.open('', '_self');
         window.close();
+
     }
 
     return (
